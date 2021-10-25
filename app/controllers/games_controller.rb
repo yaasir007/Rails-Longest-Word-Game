52f2a06725375ac6857cead7221076d2ac252c35
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-10-25 16:55:37 +0400

    not finish yet, need to check the character from the list and validate

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+    def score  
+        @word = params[:word]
+        
+        url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+        user_serialized = URI.open(url).read
+        user = JSON.parse(user_serialized)
+        @user_input =  user["found"]
+
+        
+
+    end
+
+    def new
+        @letters = ("A".."Z").to_a.sample(10)
+    end
+end
